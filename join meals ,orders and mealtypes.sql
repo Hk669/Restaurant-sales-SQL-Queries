ccd3989dd3a9e6fd5f8cc8bc5d140c35727dd209
HRUSHIKESH DOKALA <96101829+Hk669@users.noreply.github.com>
2023-05-28 19:40:05 +0530

    Update join meals ,orders and mealtypes.sql

--- join meals ,orders and mealtypes.sql
+++ join meals ,orders and mealtypes.sql
@@ -2,5 +2,3 @@ select *
 from meal_types
 inner join meals on meals.meal_type_id = meal_types.id
 inner join orders on orders.restaurant_id = meals.restaurant_id
---where meal_type_id = 1 and 
---serve_type_id = 3
